87dd8baf56fd2fabab782e02cfa1c2ee29655c27
hasnarifky <80144915+hasnarifky@users.noreply.github.com>
2021-05-28 15:09:54 +0700

    Update Kasir Supermarket Mandiri.py

--- Kasir Supermarket Mandiri.py
+++ Kasir Supermarket Mandiri.py
@@ -1,24 +1,62 @@
-print("~~~~~~~~~~Selamat Datang di Kasir Supermarket Mandiri~~~~~~~~~~")
-print("Pilih option :")
+import csv
+
+datamember = []
+
+with open('datamembership.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        datamember.append(row)
+
+# print(datamember)
+
+print("======== Selamat Datang di Kasir Supermarket Mandiri ========")
 print("1. Login member")
 print("2. Buat member")
 print("3. Transaksi tanpa member")
-option=int(input("Silahkan pilih option: "))
-if option==1:
+
+option = int(input("\nMasukkan pilihan anda (1/2/3) : "))
+if option == 1 :
     kode_member = int(input("Masukkan kode member anda: "))
-    print("Kode member anda adalah:",  kode_member)
-elif option==2:
+    # for datadiri in datamember[1:]:
+    #    kode_member = int(input("Masukkan kode member anda: "))
+    #    if dict["KODE MEMBER"] == kode_member :
+    #        print("Kode member :",  kode_member)
+    #    else :
+    #        print("Kode yang anda masukkan salah.")
+    #        kode_member = int(input("Masukkan kode member anda: "))
+
+elif option == 2 :
     print("Masukkan data diri anda")
     nama = str(input("Masukkan nama anda: "))
     no_ktp = int(input("Masukkan nomor KTP anda: "))
     no_hp = str(input("Masukkan nomor telepon anda: "))
     alamat = str(input("Masukkan alamat lengkap anda: "))
+    print("\n=============")
     print("Nama: ", nama)
     print("No KTP:", no_ktp)
     print("No Telepon: ", no_hp)
     print("Alamat: ", alamat)
-else:
+
+
+    from random import randint
+    for i in range(1) :
+        kode_baru = randint(1, 1000)
+        print("Kode member anda :", kode_baru)
+
+        data_baru = "\n{},{},{},{},{}".format(kode_baru,nama, no_ktp, no_hp, alamat)
+        file_bio = open("datamembership.csv", "a")
+        file_bio.write(data_baru)
+        file_bio.close()
+
+elif option == 3 :
     print("Silahkan masukkan kode dan jumlah barang")
+
+else :
+    print("Pilihan yang ada inputkan salah.")
+    option = int(input("\nMasukkan pilihan anda (1/2/3) : "))
+
+
+
 
 
 
